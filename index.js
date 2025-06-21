cb85c2f3035051d4d81c7a7a2919e93d0951b133
Harkirat Singh <harkirats524@gmail.com>
2025-06-22 02:50:30 +0530

    🛠️ Add font with correct link

--- index.js
+++ index.js
@@ -46,6 +46,10 @@ app.use(cors({
   credentials: true
 }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
